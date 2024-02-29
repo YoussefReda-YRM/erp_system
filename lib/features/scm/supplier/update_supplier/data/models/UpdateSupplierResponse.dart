c47686f57b38a4d2f9e59218427afe80c2d44cee
Youssef R. Mohamed <99486399+YoussefReda-YRM@users.noreply.github.com>
2024-02-29 23:40:07 +0200

    Revert "finish update supplier"

--- lib/features/scm/supplier/update_supplier/data/models/UpdateSupplierResponse.dart
+++ lib/features/scm/supplier/update_supplier/data/models/UpdateSupplierResponse.dart
@@ -1,46 +1,59 @@
 class UpdateSupplierResponse {
+  UpdateSupplierResponse({
+      this.status, 
+      this.message, 
+      this.data,});
+
+  UpdateSupplierResponse.fromJson(dynamic json) {
+    status = json['status'];
+    message = json['message'];
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
+  }
   int? status;
   String? message;
-  SupplierData? supplierData;
+  Data? data;
 
-  UpdateSupplierResponse({
-    this.status,
-    this.message,
-    this.supplierData,
-  });
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['status'] = status;
+    map['message'] = message;
+    if (data != null) {
+      map['data'] = data?.toJson();
+    }
+    return map;
+  }
 
-  factory UpdateSupplierResponse.fromJson(Map<String, dynamic> json) =>
-      UpdateSupplierResponse(
-        status: json['status'],
-        message: json['message'],
-        supplierData: json['data'] == null
-            ? null
-            : SupplierData.fromJson(json['data'] as Map<String, dynamic>),
-      );
 }
 
-class SupplierData {
+class Data {
+  Data({
+      this.id, 
+      this.supplierName, 
+      this.supplierEmail, 
+      this.supplierPhone, 
+      this.addedBy,});
+
+  Data.fromJson(dynamic json) {
+    id = json['id'];
+    supplierName = json['supplierName'];
+    supplierEmail = json['supplierEmail'];
+    supplierPhone = json['supplierPhone'];
+    addedBy = json['addedBy'];
+  }
   int? id;
   String? supplierName;
   String? supplierEmail;
   String? supplierPhone;
   String? addedBy;
 
-  SupplierData({
-    this.id,
-    this.supplierName,
-    this.supplierEmail,
-    this.supplierPhone,
-    this.addedBy,
-
-  });
-
-  factory SupplierData.fromJson(Map<String, dynamic> json) => SupplierData(
-      id: json['id'],
-      supplierName: json['supplierName'],
-      supplierEmail: json['supplierEmail'],
-      supplierPhone: json['supplierPhone'],
-      addedBy: json['addedBy']
-  );
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['supplierName'] = supplierName;
+    map['supplierEmail'] = supplierEmail;
+    map['supplierPhone'] = supplierPhone;
+    map['addedBy'] = addedBy;
+    return map;
+  }
 
 }
