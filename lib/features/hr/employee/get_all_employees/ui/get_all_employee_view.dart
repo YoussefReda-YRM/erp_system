5b3d7d043e59742c0fb041c019aa3bd5a90076c8
youssef <yrda3414@gmail.com>
2024-03-08 17:14:53 +0200

    before merge finish department

--- lib/features/hr/employee/get_all_employees/ui/get_all_employee_view.dart
+++ lib/features/hr/employee/get_all_employees/ui/get_all_employee_view.dart
@@ -1,4 +1,6 @@
 import 'package:erp_system/core/utils/app_router.dart';
+import 'package:erp_system/core/utils/colors_app.dart';
+import 'package:erp_system/core/utils/styles.dart';
 import 'package:erp_system/core/widgets/drawer_widget/drawer_widget.dart';
 import 'package:erp_system/core/widgets/drawer_widget/hr_drawer_item_list_view.dart';
 import 'package:erp_system/features/hr/employee/get_all_employees/ui/widgets/get_all_employee_view_body.dart';
@@ -27,11 +29,50 @@ class _GetAllDepartmentViewState extends State<GetAllEmployeeView> {
       body: GetAllEmployeeViewBody(
         scaffoldKey: scaffoldKey,
       ),
-      floatingActionButton: CustomFloatingActionButton(
-        ctxt: context,
-        onPressed: () {
-          GoRouter.of(context).push(AppRouter.kAddEmployeeView);
-        },
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 40),
+        child: Row(
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(12),
+                  ),
+                ),
+                backgroundColor: ColorsApp.primaryColor,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.picture_as_pdf,
+                      color: ColorsApp.lightGrey,
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Text(
+                      "Export To PDF",
+                      style: Styles.font18LightGreyBold(context)
+                          .copyWith(fontSize: 12),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Spacer(),
+            CustomFloatingActionButton(
+              ctxt: context,
+              onPressed: () {
+                GoRouter.of(context).push(AppRouter.kAddEmployeeView);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
